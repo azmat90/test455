64ec5a1d8cd14d89b1a28c75f5c1edca37312f7a
azmatalii0 <113326889+azmatalii0@users.noreply.github.com>
2025-02-17 16:32:09 +0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`┃💗⊹ 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n┃💗⊹ 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃💗⊹ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '┃💗⊹ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ ⚡𝙈𝘼𝙉𝙉𝙉𝙊-𝙈𝘿⚡ ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(` *${groupMetadata.subject}*\n\n *${participants.length}*${text ? `\n ${text}\n` : ''}\n\n\n` + users.map(v => ' @' + v.replace(/@.+/, '')).join`\n` + '\n', null, {
         mentions: users
     })
 }
